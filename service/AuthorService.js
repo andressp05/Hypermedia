651295c903ee9c9ce231f04d0c27d2894dd136ea
Luca-Colombo <lucacolo94@gmail.com>
2019-06-06 11:15:37 +0200

    fixed author object returned by ws

--- service/AuthorService.js
+++ service/AuthorService.js
@@ -30,14 +30,14 @@ exports.authorsGET = async function (offset = 0, limit = 20) {
   // let authors = sqlDb.select().table('authors').limit(limit).offset(offset);
   try {
     const data = await sqlDb.select(`${Data.Tables.author}.*`,
-      sqlDb.raw(`array_agg(${Data.Tables.book}."ISBN") AS "ISBN"`),
-      sqlDb.raw(`array_agg(${Data.Tables.book}.name) book_name`))
+      sqlDb.raw(`array_agg(${Data.Tables.book}."ISBN" ORDER BY ${Data.Tables.book}."ISBN") AS book_isbn`),
+      sqlDb.raw(`array_agg(${Data.Tables.book}.name ORDER BY ${Data.Tables.book}."ISBN") book_name`))
       .from(Data.Tables.author)
       .leftJoin(Data.Tables.written_by, `${Data.Tables.author}.author_id`, `${Data.Tables.written_by}.author_id`)
       .leftJoin(Data.Tables.book, `${Data.Tables.written_by}.ISBN`, `${Data.Tables.book}.ISBN`)
       .groupBy(`${Data.Tables.author}.author_id`).limit(limit).offset(offset);
     return new Promise((resolve, reject) => {
-      resolve(data);
+      resolve(mapAuthor(data));
     });
   } catch (e) {
     return new Promise((res, rej) => {
@@ -57,8 +57,8 @@ exports.authorsGET = async function (offset = 0, limit = 20) {
 exports.getAuthorById = async function (authorId) {
   try{
     const data = await sqlDb.select(`${Data.Tables.author}.*`,
-      sqlDb.raw(`array_agg(${Data.Tables.book}."ISBN") AS "ISBN"`),
-      sqlDb.raw(`array_agg(${Data.Tables.book}.name) book_name`))
+      sqlDb.raw(`array_agg(${Data.Tables.book}."ISBN" ORDER BY ${Data.Tables.book}."ISBN") AS book_isbn`),
+      sqlDb.raw(`array_agg(${Data.Tables.book}.name ORDER BY ${Data.Tables.book}."ISBN") book_name`))
       .from(Data.Tables.author)
       .leftJoin(Data.Tables.written_by, `${Data.Tables.author}.author_id`, `${Data.Tables.written_by}.author_id`)
       .leftJoin(Data.Tables.book, `${Data.Tables.written_by}.ISBN`, `${Data.Tables.book}.ISBN`)
@@ -66,7 +66,7 @@ exports.getAuthorById = async function (authorId) {
 
     return new Promise(function (resolve, reject) {
       if (Object.keys(data).length > 0) {
-        resolve(data);
+        resolve(mapAuthor(data));
       } else {
         reject(utils.respondWithCode(Codes.NOT_FOUND, '{"message": "Author not found"}'));
       }
@@ -77,4 +77,20 @@ exports.getAuthorById = async function (authorId) {
       rej(utils.respondWithCode(Codes.GENERIC_ERROR, `{"message": "${e}"`))
     })
   }
+}
+
+function mapAuthor(data){
+  return data.map(e => {
+    e.books = [];
+    for (let i = 0; i < e.book_isbn.length; i++) {
+      let obj = {
+        ISBN: e.book_isbn[i],
+        name: e.book_name[i]
+      };
+      e.books.push(obj);
+    }
+    delete e.book_isbn;
+    delete e.book_name;
+    return e;
+  });
 }
