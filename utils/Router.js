de1631885a3353f94ace568d9a0f0fce33f1d5da
Luca-Colombo <lucacolo94@gmail.com>
2019-06-20 22:31:27 +0200

    Added the source code zip file

--- utils/Router.js
+++ utils/Router.js
@@ -11,6 +11,10 @@ router.get('/backend/documentation.html', function(req, res, next){
   res.sendFile(path.join(__dirname, '../backend', 'documentation.html'));
 });
 
+router.get('/backend/source.zip', function(req, res, next){
+  res.sendFile(path.join(__dirname, '../backend', 'source.zip'));
+});
+
 /* NOT USEFULL THIS ARE ALREADY INCLUDED IN THE STATIC FILE HANDLING */
 // router.get('/pages/books.html', function(req, res, next){
 //   res.sendFile(path.join(__dirname, '../public', 'pages', 'books.html'));
